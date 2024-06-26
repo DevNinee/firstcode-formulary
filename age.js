135c113628491723f192df849c91546889166d99
Dev Voyager <86367089+DevNinee@users.noreply.github.com>
2024-06-25 23:57:59 -0300

    Create age.js

---
+++ age.js
@@ -0,0 +1,16 @@
+var name = ""
+var nameusuario = prompt("what is your name?")
+parseInt(nameusuario)
+nameusuario= nameusuario
+alert(nameusuario)
+
+var ageusuario = prompt("what your age?")
+var ageusuario = parseInt(ageusuario)
+age= 21 + 1
+alert(age)
+age = age + 1
+alert(age)
+agenumber = agenumber + 1
+alert(ageusuarionumber)
+alert(name)
+
